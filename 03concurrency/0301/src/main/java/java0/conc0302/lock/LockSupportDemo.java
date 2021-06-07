5cbdbc5f5f8ba776afa4ea0db3ce535d2bfffabc
chenxi <chenxi.et@gmail.com>
2021-06-07 22:30:55 +0800

    update LockSupportDemo

--- 03concurrency/0301/src/main/java/java0/conc0302/lock/LockSupportDemo.java
+++ 03concurrency/0301/src/main/java/java0/conc0302/lock/LockSupportDemo.java
@@ -3,27 +3,29 @@ package java0.conc0302.lock;
 import java.util.concurrent.locks.LockSupport;
 
 public class LockSupportDemo {
-    
+
     public static Object u = new Object();
     static ChangeObjectThread t1 = new ChangeObjectThread("t1");
     static ChangeObjectThread t2 = new ChangeObjectThread("t2");
-    
+
     public static class ChangeObjectThread extends Thread {
         public ChangeObjectThread(String name) {
             super(name);
         }
-        @Override public void run() {
+
+        @Override
+        public void run() {
             synchronized (u) {
                 System.out.println("in " + getName());
                 LockSupport.park();
                 if (Thread.currentThread().isInterrupted()) {
-                    System.out.println("被中断了");
+                    System.out.println(Thread.currentThread().getName() + " 被中断了");
                 }
-                System.out.println("继续执行");
+                System.out.println(Thread.currentThread().getName() + " 继续执行");
             }
         }
     }
-    
+
     public static void main(String[] args) throws InterruptedException {
         t1.start();
         Thread.sleep(1000L);
@@ -31,7 +33,11 @@ public class LockSupportDemo {
         Thread.sleep(3000L);
         t1.interrupt();
         LockSupport.unpark(t2);
+
+        // 主线程等待t1线程执行完
         t1.join();
+
+        // 主线程等待t2线程执行完
         t2.join();
     }
 }
